53692d6be89f568f34049c5ba16b0d91d7ec7128
BrennB99 <ben.brennan249@gmail.com>
2022-11-11 13:53:37 +0000

    completed 1 - 4 of javascript exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  args.forEach((x) => {
+    argIndex = array.indexOf(x);
+    if (argIndex >= 0) {
+      array.splice(argIndex, 1);
+    }
+  });
+  console.log(array);
+  return array;
 };
 
 // Do not edit below this line
